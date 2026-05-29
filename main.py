ba23cf9a51f9420d5b28c4826a911aee69f753db
Nick <166885937+FelixFalling@users.noreply.github.com>
2026-05-29 07:48:31 -0700

    feat: printing board

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ class tic_tac_toe_board():
         self.board[x][y] = player_marker
         self.print_board()
 
+    def check_win_codition(self):
+        pass
+
     def print_board(self):
-        for i in range(len(self.board)):
-            for j in range(len(self.board)):
-                print(self.board[i][j])
-
-
-
-
-
+      for i, row in enumerate(self.board):
+          print(" | ".join(" " if cell is None else str(cell) for cell in row))
+          if i < 2:
+              print("---------")
 def main():
     my_board = tic_tac_toe_board()
-    my_board.mark_board_at_location(2,2,"x")
+    #my_board.mark_board_at_location(1,1,"x")
+
+    for i in range(10):
+        row = int(input("Select Row:"))
+        column = int(input("Select Column:"))
+        if i % 2 == 0:
+            player = "x"
+        else:
+            player = "o"
+
+        my_board.mark_board_at_location(row,column,player)
 
 
     
