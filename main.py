b0714d48676fc0a456ab9b4d4e11c3db8effd13e
Nick <166885937+FelixFalling@users.noreply.github.com>
2026-05-29 10:11:10 -0700

    feat:basic game logic for rounds

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class tic_tac_toe_board():
-    def __init__(self):
-        #self.board[3][3] = None
-        self.board = [[None for i in range(3)] for j in range(3)]
+    def __init__(self,rows,columns):
+        self.rows = rows
+        self.columns = columns
+        self.board = [[None for i in range(rows)] for j in range(columns)]
 
 
     def mark_board_at_location(self,x,y,player_marker):
@@ -11,24 +12,32 @@ class tic_tac_toe_board():
     def check_win_codition(self):
         pass
 
+    def game_loop(self):
+        game_rounds = ((self.rows)*(self.columns))
+
+        for i in range(game_rounds):
+            row = int(input("Select Row:")) - 1
+            column = int(input("Select Column:")) - 1
+            if row not in range(len(self.board)) or column not in range(len(self.board[0])):
+                print("Input Too Large")
+                continue
+            if i % 2 == 0:
+                player = "x"
+            else:
+                player = "o"
+
+            self.mark_board_at_location(row,column,player)
+
     def print_board(self):
       for i, row in enumerate(self.board):
-          print(" | ".join(" " if cell is None else str(cell) for cell in row))
-          if i < 2:
-              print("---------")
+          current_row = (" | ".join(" " if cell is None else str(cell) for cell in row))
+          print(current_row)
+          #if i % 2 == 0:
+          print((len(current_row)*"-"))
+
 def main():
-    my_board = tic_tac_toe_board()
-    #my_board.mark_board_at_location(1,1,"x")
-
-    for i in range(10):
-        row = int(input("Select Row:"))
-        column = int(input("Select Column:"))
-        if i % 2 == 0:
-            player = "x"
-        else:
-            player = "o"
-
-        my_board.mark_board_at_location(row,column,player)
+    my_board = tic_tac_toe_board(2,2)
+    my_board.game_loop()
 
 
     
